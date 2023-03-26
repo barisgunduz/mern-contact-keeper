aad7ff832758234a79af4de5bd4d4e86a114d371
Barış Gündüz <baris.gunduz@devstup.org>
2023-03-26 16:27:41 +0300

    Feature - Register & Login Forms

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
@@ -23,6 +25,16 @@ const App = () => {
 									exact
 									path="/about"
 									element={<About />}
+								/>
+								<Route
+									exact
+									path="/register"
+									element={<Register />}
+								/>
+								<Route
+									exact
+									path="/login"
+									element={<Login />}
 								/>
 							</Routes>
 						</div>
