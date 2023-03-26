532d105824c9952794c721b72b7a329444b684a2
Barış Gündüz <baris.gunduz@devstup.org>
2023-03-26 22:33:58 +0300

    Feature - Load User & Set Token

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import AlertContext from "../../context/alert/alertContext";
 import AuthContext from "../../context/auth/authContext";
 
@@ -8,14 +9,20 @@ const Register = () => {
 
 	const { setAlert } = alertContext;
 
-	const { register, error, clearErrors } = authContext;
+	const { register, error, clearErrors, isAuthenticated } = authContext;
+
+    const navigate = useNavigate();
 
 	useEffect(() => {
+		if (isAuthenticated) {
+			navigate('/');
+		}
 		if (error === "User already exists") {
 			setAlert(error, "danger");
 			clearErrors();
 		}
-	}, [error]);
+		// eslint-disable-next-line
+	}, [error, isAuthenticated]);
 
 	const [user, setUser] = useState({
 		name: "",
