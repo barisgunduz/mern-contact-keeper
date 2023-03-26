0694d5fe0ca5d167c4422da909e297af7900ddf7
Barış Gündüz <baris.gunduz@devstup.org>
2023-03-26 22:47:52 +0300

    Feature - User Login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import AlertContext from "../../context/alert/alertContext";
+import AuthContext from "../../context/auth/authContext";
 
 const Login = () => {
+	const alertContext = useContext(AlertContext);
+	const authContext = useContext(AuthContext);
+
+	const { setAlert } = alertContext;
+
+	const { login, error, clearErrors, isAuthenticated } = authContext;
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (isAuthenticated) {
+			navigate("/");
+		}
+		if (error === "Invalid credentials") {
+			setAlert(error, "danger");
+			clearErrors();
+		}
+		// eslint-disable-next-line
+	}, [error, isAuthenticated]);
+
 	const [user, setUser] = useState({
 		email: "",
 		password: "",
@@ -12,7 +34,14 @@ const Login = () => {
 		setUser({ ...user, [e.target.name]: e.target.value });
 	const onSubmit = (e) => {
 		e.preventDefault();
-		console.log("Login Submit");
+		if (email === "" || password === "") {
+			setAlert("Please fill in all fields", "danger");
+		} else {
+			login({
+				email,
+				password,
+			});
+		}
 	};
 
 	return (
@@ -23,14 +52,22 @@ const Login = () => {
 			<form onSubmit={onSubmit}>
 				<div className="form-group">
 					<label htmlFor="email">Email</label>
-					<input type="email" name={email} onChange={onChange} />
+					<input
+						type="email"
+                        name="email"
+						value={email}
+						onChange={onChange}
+						required
+					/>
 				</div>
 				<div className="form-group">
 					<label htmlFor="password">Password</label>
 					<input
 						type="password"
-						name={password}
+                        name="password"
+						value={password}
 						onChange={onChange}
+						required
 					/>
 				</div>
 				<input
