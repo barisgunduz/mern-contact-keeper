deb1365f0783265eb75306278754f97402b21543
Barış Gündüz <baris.gunduz@devstup.org>
2023-03-27 22:30:29 +0300

    Feature - Add COntact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from "react";
-import {v4} from "uuid";
+import axios from "axios";
 import ContactContext from "./contactContext";
 import contactReducer from "./contactReducer";
 import {
@@ -10,43 +10,31 @@ import {
 	UPDATE_CONTACT,
 	FILTER_CONTACTS,
 	CLEAR_FILTER,
+	CONTACT_ERROR
 } from "../types";
 
 const ContactState = (props) => {
 	const initialState = {
-		contacts: [
-			{
-				id: 1,
-				name: "Ted Smith",
-				email: "ted@gmail.com",
-				phone: "222-111-1111",
-				type: "personal",
-			},
-			{
-				id: 2,
-				name: "ZED Smith",
-				email: "zed@gmail.com",
-				phone: "222-333-1111",
-				type: "personal",
-			},
-			{
-				id: 3,
-				name: "CAT Smith",
-				email: "cat@gmail.com",
-				phone: "222-444-1111",
-				type: "professional",
-			},
-		],
+		contacts: [],
 		current: null,
-		filtered: null
+		filtered: null,
+		error: null
 	};
 
 	const [state, dispatch] = useReducer(contactReducer, initialState);
 
 	// Add Contact
-	const addContact = (contact) => {
-		contact.id = v4();
-		dispatch({ type: ADD_CONTACT, payload: contact });
+	const addContact = async (contact) => {
+		const config = {
+			headers: { "Content-Type": "application/json" },
+		};
+
+		try {
+			const res = await axios.post("/api/contacts", contact, config);
+			dispatch({ type: ADD_CONTACT, payload: res.data });
+		} catch (err) {
+			dispatch({ type: CONTACT_ERROR , payload: err.response.msg});
+		}
 	};
 
 	// Delete Contact
@@ -85,13 +73,14 @@ const ContactState = (props) => {
 				contacts: state.contacts,
 				current: state.current,
 				filtered: state.filtered,
+				error: state.error,
 				addContact,
 				deleteContact,
 				setCurrent,
 				clearCurrent,
 				updateContact,
 				filterContacts,
-				clearFilter
+				clearFilter,
 			}}
 		>
 			{props.children}
